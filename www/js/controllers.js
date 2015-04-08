b7148a04d8d431439e35ecdbbf8f152ce80b2572
Fengda HUANG <h@phodal.com>
2015-04-08 23:48:37 +0800

    Format code & remove iframe

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('cordpress.controllers', ['cordpress.services'])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout, $sce, DataLoader, $rootScope) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $sce, DataLoader, $rootScope) {
 
     $rootScope.url = 'http://www.leanjs.net';
 
@@ -8,46 +8,37 @@ angular.module('cordpress.controllers', ['cordpress.services'])
 
     $ionicModal.fromTemplateUrl('templates/login.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.modal = modal;
-      $scope.src = $sce.trustAsResourceUrl( $rootScope.url + '/?appp=login' );
     });
 
-    $scope.closeLogin = function() {
+    $scope.closeLogin = function () {
       $scope.modal.hide();
     };
 
-    $scope.login = function() {
+    $scope.login = function () {
       $scope.modal.show();
     };
 
-    $scope.logUserIn = function( username, password ) {
+    $scope.logUserIn = function (username, password) {
 
       console.log('Logging in...');
 
       $scope.spinner = true;
       $scope.loginMessage = null;
 
-      targetFrame = window.frames['login-iframe'];
-      targetFrame.postMessage( {
-        message: 'login',
-        username: username,
-        password: password
+    };
 
-      }, '*');
-
-    }
-
-    window.addEventListener('message', function(event) {
-      if( event.data.loggedin === true ) {
+    window.addEventListener('message', function (event) {
+      if (event.data.loggedin === true) {
         $scope.spinner = false;
         console.log(event.data);
         $scope.loggedin();
         $scope.closeLogin();
-        localStorage.setItem('reactorUser', JSON.stringify( event.data ) );
+        localStorage.setItem('reactorUser', JSON.stringify(event.data));
       }
 
-      if( event.data.loggedin === false )  {
+      if (event.data.loggedin === false) {
         $scope.spinner = false;
         console.log(event.data.message);
         $scope.loginMessage = event.data.message;
@@ -56,58 +47,58 @@ angular.module('cordpress.controllers', ['cordpress.services'])
 
     });
 
-    $scope.loggedin = function() {
+    $scope.loggedin = function () {
       $scope.isUserLoggedIn = true;
-    }
+    };
 
-    $scope.logUserOut = function() {
+    $scope.logUserOut = function () {
       $scope.$broadcast('logout');
-    }
+    };
 
-    $scope.$on('logout', function(event, msg) {
+    $scope.$on('logout', function (event, msg) {
       console.log('doing logout');
       localStorage.removeItem('reactorUser');
       $scope.isUserLoggedIn = false;
       $scope.closeLogin();
     });
-})
+  })
 
-.controller('PostCtrl', function ($scope, $stateParams, DataLoader, $ionicLoading, $rootScope, $sce) {
-	$rootScope.url = 'http://www.leanjs.net';
+  .controller('PostCtrl', function ($scope, $stateParams, DataLoader, $ionicLoading, $rootScope, $sce) {
+    $rootScope.url = 'http://www.leanjs.net';
 
-	$rootScope.callback = '_jsonp=JSON_CALLBACK';
+    $rootScope.callback = '_jsonp=JSON_CALLBACK';
 
-	$ionicLoading.show({
-		noBackdrop: true
-	});
+    $ionicLoading.show({
+      noBackdrop: true
+    });
 
-	var singlePostApi = $rootScope.url + '/wp-json/posts/' + $stateParams.postId + '?' + $rootScope.callback;
+    var singlePostApi = $rootScope.url + '/wp-json/posts/' + $stateParams.postId + '?' + $rootScope.callback;
 
-	DataLoader.get(singlePostApi).success(function (data, status, headers, config) {
-		$scope.post = data;
-		$scope.content = $sce.trustAsHtml(data.content);
-		$ionicLoading.hide();
-	}).error(function (data, status, headers, config) {
-		console.log('error');
-	});
+    DataLoader.get(singlePostApi).success(function (data, status, headers, config) {
+      $scope.post = data;
+      $scope.content = $sce.trustAsHtml(data.content);
+      $ionicLoading.hide();
+    }).error(function (data, status, headers, config) {
+      console.log('error');
+    });
 
-})
+  })
 
-.controller('PostsCtrl', function ($scope, $stateParams, DataLoader, $ionicLoading, $rootScope, $sce) {
-	$rootScope.url = 'http://www.leanjs.net';
+  .controller('PostsCtrl', function ($scope, $stateParams, DataLoader, $ionicLoading, $rootScope, $sce) {
+    $rootScope.url = 'http://www.leanjs.net';
 
-	$rootScope.callback = '_jsonp=JSON_CALLBACK';
+    $rootScope.callback = '_jsonp=JSON_CALLBACK';
 
-	$ionicLoading.show({
-		noBackdrop: true
-	});
+    $ionicLoading.show({
+      noBackdrop: true
+    });
 
-	var singlePostApi = $rootScope.url + '/wp-json/posts/' + '?' + $rootScope.callback;
+    var singlePostApi = $rootScope.url + '/wp-json/posts/' + '?' + $rootScope.callback;
 
-	DataLoader.get(singlePostApi).success(function (data, status, headers, config) {
-		$scope.posts = data;
-		$ionicLoading.hide();
-	}).error(function (data, status, headers, config) {
-		console.log('error');
-	});
-});
+    DataLoader.get(singlePostApi).success(function (data, status, headers, config) {
+      $scope.posts = data;
+      $ionicLoading.hide();
+    }).error(function (data, status, headers, config) {
+      console.log('error');
+    });
+  });
