41f63b47cbb671ccc9b8f191b39c9cbdf96f5a3b
Fengda HUANG <h@phodal.com>
2015-04-08 21:01:47 +0800

    remove some unuse logic

--- www/js/app.js
+++ www/js/app.js
@@ -1,71 +1,44 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('cordpress', ['ionic', 'cordpress.controllers', 'cordpress.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
-    }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.posts', {
-      url: "/posts",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/posts.html",
-          controller: 'PostsCtrl'
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+      })
+
+      .state('app.posts', {
+        url: "/posts",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/posts.html",
+            controller: 'PostsCtrl'
+          }
         }
-      }
-    })
+      })
 
-  .state('app.single', {
-    url: "/posts/:postId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/post_detail.html",
-        controller: 'PostCtrl'
-      }
-    }
+      .state('app.single', {
+        url: "/posts/:postId",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/post_detail.html",
+            controller: 'PostCtrl'
+          }
+        }
+      });
+    $urlRouterProvider.otherwise('/app/posts');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/posts');
-});
